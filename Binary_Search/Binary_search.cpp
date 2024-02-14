5b02c6cb1aaad1f0c34e75761f14f2b22ecd5785
Muhammad Alamgir Kabir <117192967+Alamgir689@users.noreply.github.com>
2024-02-14 13:04:01 +0600

    Add files via upload

---
+++ Binary_Search/Binary_search.cpp
@@ -0,0 +1,106 @@
+/*
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->val << endl;
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->val << endl;
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << endl;
+}
+
+void level_order(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << endl;
+
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    h->right = i;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+
+    // Calling preorder function for printing
+    preorder(root);
+
+    // Calling preorder function for printing
+    postorder(root);
+    // Calling preorder function for printing
+    inorder(root);
+    // Level order traversal
+    level_order(root);
+
+    return 0;
+}
